46728982e5de3efcc1a3747521269b0494e5dd69
Cheryl Zhang <zhanc573@newschool.edu>
2019-03-14 00:31:24 -0400

    add

---
+++ forgotten/script.js
@@ -0,0 +1,16 @@
+//    var body = document.getElementsByTagName("body")[0];
+//    
+//    var lastElement = document.getElementById("body")
+//    
+//    body.onscroll= function(){
+//    
+//        //get scroll top 
+//        
+//        scrollPosition = body.scrollTop;
+//        
+//        //if(scrollPosition > our element's offset.y value){
+//            
+//            lastElement.style.opacity = 0;
+//            
+//        }
+//    
